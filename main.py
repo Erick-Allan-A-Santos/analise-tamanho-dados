ff224c61ee8718458bfb14ba4edb08edc9f22173
Erick Allan <erick.allan.a.santos@gmail.com>
2026-09-26 13:17:00 -0300

    feat: adiciona endpoint para receber dados

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Olá, mundo!")
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+    return "Servidor funcionando!"
+
+
+@app.route("/teste", methods=["POST"])
+def teste():
+    dados = request.get_data()
+
+    tamanho = len(dados)
+
+    return f"Tamanho recebido: {tamanho} bytes"
+
+if __name__ == "__main__":
+    app.run(debug=True)
